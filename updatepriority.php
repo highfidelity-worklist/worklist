2e2c934fb65e7b6661412111a46c2b9a6be7002d
leonty <leonty@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-02-22 15:09:35 +0000

    Change the way item position is calculated/reported when updating priority - now it calculates it's actual position in the list (for BIDDING items).
    author: Leonty
    review: Leonty
    commit: Leonty

--- updatepriority.php
+++ updatepriority.php
@@ -18,7 +18,6 @@ if (!isset($_SESSION['userid'])) {
 
 $id = isset($_REQUEST["id"]) ? intval($_REQUEST["id"]) : -1;
 $previd = isset($_REQUEST["previd"]) ? intval($_REQUEST["previd"]) : -1;
-$bump = isset($_REQUEST["bump"]) ? intval($_REQUEST["bump"]) : 0;
 if ($id < 0 || $previd < 0) {
     echo 'error: args';
     return;
@@ -27,18 +26,25 @@ if ($id < 0 || $previd < 0) {
 $query = "set @pri := -1";
 $rt = mysql_query($query);
 
+// calculating position change
+$query = "SET @rownum := 0";
+mysql_unbuffered_query($query);
+
 if ($rt) {
     $query = "update ".WORKLIST." set priority=(@pri := @pri + 2) where 1 order by priority";
     $rt = mysql_query($query);
 }
 
 if ($rt) {
-    $query = "select summary, priority from ".WORKLIST." where id='$id'";
+
+    $query = "SELECT summary, priority, position FROM (
+SELECT *, @rownum := @rownum+1 AS position FROM `".WORKLIST."` WHERE `status` = 'BIDDING' ORDER BY `priority` ) AS position_table WHERE id = '$id'";
     $rt = mysql_query($query);
     if ($rt) {
         $row = mysql_fetch_assoc($rt);
         $summary = $row['summary'];    
         $origpri = $row['priority'];
+        $origpos = $row['position'];
     }
 }
 
@@ -61,12 +67,21 @@ if ($rt) {
 }
 
 if ($rt) {
-    if ($bump > 0) $bump = "+$bump";
+
+    // getting position afterwards
+    $query = "SET @rownum := 0";
+    mysql_unbuffered_query($query);
+
+    $query = "SELECT position FROM (
+SELECT *, @rownum := @rownum+1 AS position FROM `".WORKLIST."` WHERE `status` = 'BIDDING' ORDER BY `priority` ) AS position_table WHERE id = '$id'";
+    $rt = mysql_query($query);
+    $row = mysql_fetch_assoc($rt);
+    $newpos = $row['position'];
 
     $data = array();
     $data['user'] = JOURNAL_API_USER;
     $data['pwd'] = sha1(JOURNAL_API_PWD);
-    $data['message'] = $_SESSION['nickname'] . " moved \"$summary\" from position $origpri > position $newpri";
+    $data['message'] = $_SESSION['nickname'] . " moved \"$summary\" from position $origpos to position $newpos";
     $prc = postRequest(JOURNAL_API_URL, $data);
 } else {
     echo 'error: database';
