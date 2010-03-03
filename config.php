7e3ecaf13648f7c59a92755f1feb8f72f774e228
gizmo <gizmo@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-03-03 12:45:30 +0000

    Implemented 2 Atom/RSS Feeds: Job#10266

--- config.php
+++ config.php
@@ -4,68 +4,69 @@
 //  All Rights Reserved. 
 //  http://www.lovemachineinc.com
 //
+require_once('Zend/Config.php');
+require_once('Zend/Registry.php');
 
 if (file_exists('server.local.php')) {
     include_once('server.local.php');
 }
 
-if (!defined("APP_NAME"))       define("APP_NAME","Worklist");
-if (!defined("APP_LOCATION"))   define("APP_LOCATION",substr($_SERVER['SCRIPT_NAME'], 1, strrpos($_SERVER['SCRIPT_NAME'], "/")));
-if (!defined("APP_BASE"))       define("APP_BASE",substr(APP_LOCATION, 0, strrpos(APP_LOCATION, "/", -2)));
+if (!defined('APP_NAME'))       define('APP_NAME','Worklist');
+if (!defined('APP_LOCATION'))   define('APP_LOCATION',substr($_SERVER['SCRIPT_NAME'], 1, strrpos($_SERVER['SCRIPT_NAME'], '/')));
+if (!defined('APP_BASE'))       define('APP_BASE',substr(APP_LOCATION, 0, strrpos(APP_LOCATION, '/', -2)));
 if (!defined('APP_PATH'))	define('APP_PATH', realpath(dirname(__FILE__)));
 if (!defined('UPLOAD_PATH'))	define('UPLOAD_PATH', realpath(APP_PATH . '/uploads'));
 
 if (!defined('APP_ENV'))	define('APP_ENV', 'production');
 
 //http[s]://[[SECURE_]SERVER_NAME]/[LOCATION/]index.php   #Include a TRAILING / if LOCATION is defined
-if (!defined("SERVER_NAME"))    define("SERVER_NAME","dev.sendlove.us");
-if (!defined("SERVER_URL"))     define("SERVER_URL",'http://'.SERVER_NAME.'/'.APP_LOCATION); //Include [:port] for standard http traffic if not :80
-if (!defined("SERVER_BASE"))    define("SERVER_BASE",'http://'.SERVER_NAME.'/'.APP_BASE);
+if (!defined('SERVER_NAME'))    define('SERVER_NAME','dev.sendlove.us');
+if (!defined('SERVER_URL'))     define('SERVER_URL','http://'.SERVER_NAME.'/'.APP_LOCATION); //Include [:port] for standard http traffic if not :80
+if (!defined('SERVER_BASE'))    define('SERVER_BASE','http://'.SERVER_NAME.'/'.APP_BASE);
 //SSL Not enabled on development
 //define("SECURE_SERVER_URL",'https://'.SERVER_NAME.'/'.APP_LOCATION); //Secure domain defaults to standard; Include [:port] for secure https traffic if not :443
 //So clone the standard URL
-if (!defined("SECURE_SERVER_URL")) define("SECURE_SERVER_URL",SERVER_URL); //Secure domain defaults to standard; Include [:port] for secure https traffic if not :443
+if (!defined('SECURE_SERVER_URL')) define('SECURE_SERVER_URL',SERVER_URL); //Secure domain defaults to standard; Include [:port] for secure https traffic if not :443
 
-if (!defined("FEEDBACK_EMAIL")) define("FEEDBACK_EMAIL","feedback@lovemachineinc.com");
+if (!defined('FEEDBACK_EMAIL')) define('FEEDBACK_EMAIL','feedback@lovemachineinc.com');
 
-if (!defined("DB_SERVER"))      define("DB_SERVER", "localhost");
-if (!defined("DB_USER"))        define("DB_USER", "project_tofor");
-if (!defined("DB_PASSWORD"))    define("DB_PASSWORD", "test30");
-if (!defined("DB_NAME"))        define("DB_NAME", "worklist_dev");
+if (!defined('DB_SERVER'))      define('DB_SERVER', 'localhost');
+if (!defined('DB_USER'))        define('DB_USER', 'project_tofor');
+if (!defined('DB_PASSWORD'))    define('DB_PASSWORD', 'test30');
+if (!defined('DB_NAME'))        define('DB_NAME', 'worklist_dev');
 
-if (!defined("WORKLIST"))       define("WORKLIST", "worklist");
-if (!defined("USERS"))          define("USERS", "users");
-if (!defined("BIDS"))          define("BIDS", "bids");
-if (!defined("FEES"))          define("FEES", "fees");
-if (!defined("FILES"))		define("FILES", "files");
+if (!defined('WORKLIST'))       define('WORKLIST', 'worklist');
+if (!defined('USERS'))          define('USERS', 'users');
+if (!defined('BIDS'))          define('BIDS', 'bids');
+if (!defined('FEES'))          define('FEES', 'fees');
+if (!defined('FILES'))		define('FILES', 'files');
 
-if (!defined("SALT"))           define("SALT", "WORKLIST");
-if (!defined("SESSION_EXPIRE")) define("SESSION_EXPIRE", 365*24*60*60);
-if (!defined("REQUIRELOGINAFTERCONFIRM")) define("REQUIRELOGINAFTERCONFIRM", 1);
+if (!defined('SALT'))           define('SALT', 'WORKLIST');
+if (!defined('SESSION_EXPIRE')) define('SESSION_EXPIRE', 365*24*60*60);
+if (!defined('REQUIRELOGINAFTERCONFIRM')) define('REQUIRELOGINAFTERCONFIRM', 1);
 
-if (!defined("JOURNAL_API_URL"))     define("JOURNAL_API_URL", "https://dev.sendlove.us/journal/add.php");
-if (!defined("JOURNAL_API_USER"))    define("JOURNAL_API_USER", "api_username");
-if (!defined("JOURNAL_API_PWD"))     define("JOURNAL_API_PWD", "api_password");
+if (!defined('JOURNAL_API_URL'))     define('JOURNAL_API_URL', 'https://dev.sendlove.us/journal/add.php');
+if (!defined('JOURNAL_API_USER'))    define('JOURNAL_API_USER', 'api_username');
+if (!defined('JOURNAL_API_PWD'))     define('JOURNAL_API_PWD', 'api_password');
 
 // Refresh interval for ajax updates of the history table (in seconds)
-if (!defined("AJAX_REFRESH"))   define("AJAX_REFRESH", 30);
+if (!defined('AJAX_REFRESH'))   define('AJAX_REFRESH', 30);
 
 //pagination vars
-if (!defined("QS_VAR"))         define("QS_VAR", "page");
+if (!defined('QS_VAR'))         define('QS_VAR', 'page');
 
-if (!defined("STR_FWD"))        define("STR_FWD", "&nbsp;&nbsp;Next");
-if (!defined("STR_BWD"))        define("STR_BWD", "Prev&nbsp;&nbsp;");
-if (!defined("IMG_FWD"))        define("IMG_FWD", "images/left.png");
-if (!defined("IMG_BWD"))        define("IMG_BWD", "images/right.png");
-
+if (!defined('STR_FWD'))        define('STR_FWD', '&nbsp;&nbsp;Next');
+if (!defined('STR_BWD'))        define('STR_BWD', 'Prev&nbsp;&nbsp;');
+if (!defined('IMG_FWD'))        define('IMG_FWD', 'images/left.png');
+if (!defined('IMG_BWD'))        define('IMG_BWD', 'images/right.png');
 
 /*
  * Non-configuration values (CONSTANTS)
  */
 
 // User features: bits in the users.features column
-define("FEATURE_SUPER_ADMIN",	0x0001);
-define("FEATURE_USER_MASK",	    0x0001);
+define('FEATURE_SUPER_ADMIN',	0x0001);
+define('FEATURE_USER_MASK',	    0x0001);
 
 if (empty($mail_user) || !is_array($mail_user)) {
 $mail_user = array (
@@ -80,4 +81,20 @@ $mail_user = array (
   );
 }
 
-?>
+// Configuration Array
+// New keys can be added to this array, for configuration of features
+// and retrieved where needed using Zend_Registry::get('config');
+$config = array(
+    'database' => array(
+    	'name' 	  => 'Worklist',
+        'adapter' => 'mysqli',
+        'params'  => array(
+            'host'     => DB_SERVER,
+            'username' => DB_USER,
+            'password' => DB_PASSWORD,
+            'dbname'   => DB_NAME,
+        )
+    )
+);
+// New config object, allows additional merging
+Zend_Registry::set('config', new Zend_Config($config, true));
---
+++ feeds.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Copyright (c) 2010, LoveMachine Inc.
+ * All Rights Reserved. 
+ * http://www.lovemachineinc.com
+ * 
+ * Feeds.php
+ * Paramters:  name= name_of_feed (priority | completed)
+ * 			   format=atom | rss
+ */
+require_once('config.php');
+require_once('db.php');
+require_once('Zend/Feed/Writer/Feed.php');
+
+function addEntry($writer, $entryData, $entryDescription) {
+	$entry = $writer->createEntry();
+	$entry->setTitle($entryData['title']); 
+	$entry->setLink(SERVER_URL . 'workitem.php?job_id=' . $entryData['worklist_id'] . '&action=view'); 
+	//$entry->setDescription($entryDescription);
+	$entry->setDescription($entryData['content']);   
+	//$entry->setContent($entryData['content']); 
+	$entry->setDateCreated(time()); 
+	$entry->setDateModified(time()); 
+	$entry->addAuthor($entryData['author'], $entryData['email']); 
+	$writer->addEntry($entry); // manual addition post-creation 
+}
+
+function loadFeed($writer, $query, $entryDescription) {
+	$db = Zend_Registry::get('db');
+	$result = $db->fetchAll($query);
+	if ($result) {
+		foreach($result as $row) {
+			addEntry($writer, $row, $entryDescription);
+		}
+	}
+}
+
+$format = isset($_REQUEST['format']) ? $_REQUEST['format'] : 'rss';
+if (!($format == 'atom' || $format == 'rss')) {
+	$format = 'rss';
+} 
+$name = isset($_REQUEST['name']) ? $_REQUEST['name'] : 'completed';
+// Connect to db
+try {
+	$db = Zend_Registry::get('db');
+	$db->getConnection();
+} catch (Zend_Db_Adapter_Exception $e) {
+    // failed login, db not running
+    
+} catch (Zend_Exception $e) {
+	// factory() failed load of mysqli
+} 
+
+switch ($name) {
+	case 'priority' :
+		$name = 'priority';
+		$title = 'Worklist Top Priority Bidding Jobs';
+		$description = 'dev.sendlove.us Worklist, highest priority jobs Bidding';
+		$entryDescription = 'Worklist priority item';
+		$query = "SELECT w.id as worklist_id, u1.nickname as author, username as email, summary as title, notes as content
+					FROM worklist w
+					JOIN users u1 ON u1.id = w.owner_id AND w.status = 'BIDDING'
+					ORDER BY priority LIMIT 20";
+		break;
+	case 'completed' : 
+	default :
+		$name = 'completed';
+		$title = 'Worklist most Recent completed jobs';
+		$description = 'dev.sendlove.us Worklist, Most recent completed Jobs';
+		$entryDescription = 'Worklist priority item';
+		$query = "SELECT w.id as worklist_id, u1.nickname as author, username as email, summary as title, notes as content
+					FROM worklist w
+					JOIN users u1 ON u1.id = w.owner_id AND w.status = 'DONE'
+					ORDER BY created DESC LIMIT 20";
+}
+
+$url = 'http://dev.sendlove.us/';
+$link = $url . "feed.php?name=$name&format=$format";
+$writer = new Zend_Feed_Writer_Feed; 
+$writer->setTitle($title); 
+$writer->setLink(SERVER_URL); 
+$writer->setFeedLink(SERVER_URL . $link, $format);
+$writer->setDescription($description);
+$writer->setDateModified(time()); //Atom needs this  
+loadFeed($writer, $query, $entryDescription);
+
+echo $writer->export($format); 
