f1808bbf1d3f6bb3fb58b5f00a846d5af99ad687
tcrowe <tcrowe@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-03-05 01:56:34 +0000

    #10526: Re-add getworkitem.php which is used by journal.

---
+++ getworkitem.php
@@ -0,0 +1,21 @@
+<?php
+//  Copyright (c) 2010, LoveMachine Inc.  
+//  All Rights Reserved.  
+//  http://www.lovemachineinc.com
+
+// AJAX request from ourselves to retrieve history
+
+include("config.php");
+include("class.session_handler.php");
+
+$item = isset($_REQUEST["item"]) ? intval($_REQUEST["item"]) : 0;
+if (empty($item))
+    return;
+
+$query = "SELECT ".WORKLIST.".`id`, `summary`, `nickname`, `status`, `notes` FROM ".WORKLIST. 
+         " LEFT JOIN ".USERS." ON ".WORKLIST.".`owner_id` = ".USERS.".`id` WHERE ".WORKLIST.".id = '$item'";
+$rt = mysql_query($query);
+$row = mysql_fetch_assoc($rt);
+
+$json = json_encode(array( $row['summary'], $row['nickname'], $row['status'], $row['notes'], $row['id']));
+echo $json;     
