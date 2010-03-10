0c063c4c8b8ca1d95b485bc9227d7d31a9519079
aclarib <aclarib@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-03-10 07:07:00 +0000

    Small fix for format.php add function RelativeTime.

--- format.php
+++ format.php
@@ -324,6 +324,22 @@
 			$('#popup-user-info').dialog('open');
 		}
 		
+		function RelativeTime(x){
+			var plural = '';
+		 
+			var mins = 60, hour = mins * 60; day = hour * 24,
+				week = day * 7, month = day * 30, year = day * 365;
+
+			if (x >= year) { x = (x / year)|0; dformat="yr"; }
+			else if (x >= month) { x = (x / month)|0; dformat="mnth"; }
+			else if (x >= day*4) { x = (x / day)|0; dformat="day"; }
+			else if (x >= hour) { x = (x / hour)|0; dformat="hr"; }
+			else if (x >= mins) { x = (x / mins)|0; dformat="min"; }
+			else { x |= 0; dformat="sec"; }
+			if (x > 1) plural = 's';
+			if (x < 0) x = 0;
+			return x + ' ' + dformat + plural;
+		}
 		// End of user info code
 		</script>
 
