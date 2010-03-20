61f42ff59f2867145b0ae5c7c1e789baea1a2b03
seong <seong@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-03-20 17:01:14 +0000

    reji: Footer improved. #10749

--- footer.php
+++ footer.php
@@ -17,17 +17,40 @@
         <div style="float:none; clear:both;"></div>
         
 <!-- Close DIV container -->
-    </div>
-<?php if (!isset($inFeedlist)) { ?>
-	<div style="text-align:center;"><a href="feedlist.php" title="Rss & Atom Feeds"><img src="images/rss20.png"></img><img src="images/atom20.png"></img></a></div>
-<?php } ?>	
-
+  </div>
 	<div id="footer">
-    <p>&copy; <? echo date("Y"); ?> <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a> &nbsp;| &nbsp;<a href="privacy.php">Privacy Policy</a><br/><a href="http://svn.sendlove.us/" target="_blank">View the source code</a></p>
-    <img src="images/LMLogo3.png" />
+	<div class="lefticon" style="padding-top:10px;"><a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a></div>
+<div class="loves"><img src="images/LMLogo3.png"/></div>   
+ &copy; <? echo date("Y"); ?> <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a> &nbsp;| &nbsp;<a href="privacy.php">Privacy Policy</a>&nbsp;| &nbsp;<a href="http://svn.sendlove.us/" target="_blank">View the source code</a>
     </div>
+<!-- 
+    </div>
+	<div style="text-align:left; padding-left:50px;"><a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a></div>
+	<div id="footer">
+    <p>&copy; 21010 <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a> &nbsp;| &nbsp;<a href="privacy.php">Privacy Policy</a>&nbsp;| &nbsp;<a href="http://svn.sendlove.us/" target="_blank">View the source code</a></p>
+    <img src="images/LMLogo3.png"  align="right" style="padding-right:20px; padding-bottom:15px"/>
+    </div>
+	
+	added bt mechanic 
+</div style="diplay:table-row; heght:120px; width:760px; margin-left:45px">
 
-<!-- Close DIV outside -->
+	<div style="diplay:table-cell vertical-align:top;  text-align:left;">
+
+		<a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a>
+
+	</div>
+ 
+	<div style="diplay:table-cell vertical-align:middle;  text-align:center;">
+
+		&copy; 2010 <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a> &nbsp;| &nbsp;<a href="privacy.php">Privacy Policy</a>&nbsp;| &nbsp;<a href="http://svn.sendlove.us/" target="_blank">View the source code</a>
+
+	<div style="diplay:table-cell vertical-align:bottom;  text-align:right;">
+
+		<img src="images/LMLogo3.png"  align="middle"  /> 
+
+	</div>-->
+
+</div><!-- Close DIV outside -->
 </div id="outside">
 
 <!-- Google Analytics -->
