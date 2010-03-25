7d4fc5961672688ae9fab6f014d714e22d7a56a7
aneuhaus <aneuhaus@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-03-25 20:14:27 +0000

    #10790 Footer: Feed icons repositioned, View Source link takes to specific file in svn

--- footer.php
+++ footer.php
@@ -5,58 +5,30 @@
 
 ?>
 <!-- break contained floats -->        
-
-                <div style="float:none; clear:both;"></div>
+			<div style="float:none; clear:both;"></div>
 
 <!-- END MAIN BODY - Close DIV center -->
-
 		</div>
-        
-        <div id="right"></div>
+		<div id="right"></div>
+
 <!-- break 3-col float -->        
-        <div style="float:none; clear:both;"></div>
-        
+		<div style="float:none; clear:both;"></div>
+
 <!-- Close DIV container -->
 	</div>
-		<div id="footer">
-			<div class="lefticon" style="padding-top:10px;"><a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a></div>
-			<?php
-				$res = preg_split('%/%', $_SERVER['SCRIPT_NAME']);
-				$filename = array_pop($res);
-				$repname = array_pop($res);
-				$viewSourceLink = "http://svn.sendlove.us/filedetails.php?repname=".$repname."&path=/".$filename;
-			?>
-			<div class="copyText">&copy;&nbsp;<? echo date("Y"); ?> <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="privacy.php" target="_blank">Privacy Policy</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php echo $viewSourceLink;?>" target="_blank">View the source code</a></div>
-			<div class="loves"><img src="images/LMLogo3.png"/></div>
-		</div>
-<!-- 
-    </div>
-	<div style="text-align:left; padding-left:50px;"><a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a></div>
 	<div id="footer">
-    <p>&copy; 21010 <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a> &nbsp;| &nbsp;<a href="privacy.php">Privacy Policy</a>&nbsp;| &nbsp;<a href="http://svn.sendlove.us/" target="_blank">View the source code</a></p>
-    <img src="images/LMLogo3.png"  align="right" style="padding-right:20px; padding-bottom:15px"/>
-    </div>
-	
-	added bt mechanic 
-</div style="diplay:table-row; heght:120px; width:760px; margin-left:45px">
-
-	<div style="diplay:table-cell vertical-align:top;  text-align:left;">
-
-		<a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a>
-
+		<div class="lefticon" style="padding-top:10px;"><a href="feedlist.php" title="Rss & Atom Feeds" style="text-decoration:none;"><img src="images/rss20.png"></img>&nbsp;<img src="images/atom20.png"></img></a></div>
+		<?php
+			$res = preg_split('%/%', $_SERVER['SCRIPT_NAME']);
+			$filename = array_pop($res);
+			$repname = array_pop($res);
+			$viewSourceLink = "http://svn.sendlove.us/filedetails.php?repname=".$repname."&path=/".$filename;
+		?>
+		<div class="copyText">&copy;&nbsp;<? echo date("Y"); ?> <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="privacy.php" target="_blank">Privacy Policy</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php echo $viewSourceLink;?>" target="_blank">View the source code</a></div>
+		<div class="loves"><img src="images/LMLogo3.png"/></div>
 	</div>
- 
-	<div style="diplay:table-cell vertical-align:middle;  text-align:center;">
 
-		&copy; 2010 <a href="http://www.lovemachineinc.com" target="_blank">LoveMachine, Inc.</a> &nbsp;| &nbsp;<a href="privacy.php">Privacy Policy</a>&nbsp;| &nbsp;<a href="http://svn.sendlove.us/" target="_blank">View the source code</a>
-
-	<div style="diplay:table-cell vertical-align:bottom;  text-align:right;">
-
-		<img src="images/LMLogo3.png"  align="middle"  /> 
-
-	</div>-->
-
-</div><!-- Close DIV outside -->
+<!-- Close DIV outside -->
 </div id="outside">
 
 <!-- Google Analytics -->
