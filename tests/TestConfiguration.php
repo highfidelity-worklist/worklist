78b9c2e1ad1aba0e0b470ba562fa4d167efaf574
seong <seong@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-03-30 17:54:25 +0000

    Removing explicit test configuration and ignoring all local configurations.

--- tests/TestConfiguration.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * PHPUnit Code Coverage / Test Report
- */
-define('TESTS_GENERATE_REPORT', false);
-define('TESTS_GENERATE_REPORT_TARGET', dirname(__FILE__) . 'reports/code_coverage');
