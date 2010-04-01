4b2ff6d3238e4312574d595578540b751fd54da2
aclarib <aclarib@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-04-01 17:41:41 +0000

    #10871 Add ability to ping runner of a task

--- pingtask.php
+++ pingtask.php
@@ -16,6 +16,8 @@ $send_mail = false;
 if( isset( $_REQUEST['mail'] ) )
 	$send_mail = intval( $_REQUEST['mail'] );
 
+$who = $_REQUEST['who'];
+
 // Get sender Nickname
 $id = getSessionUserId();
 $user = getUserById( $id );
@@ -25,14 +27,22 @@ $email = $user->username;
 // Get item
 $item = getWorklistById( $item_id );
 
-// Get mechanic Nickname & email
-$mechanic_id = $item['mechanic_id'];
-$mechanic = getUserById( $mechanic_id );
-$mechanic_nick = $mechanic->nickname;
-$mechanic_email = $mechanic->username;
+if( $who == 'mechanic' ) {
+    // Get mechanic Nickname & email
+    $receiver_id = $item['mechanic_id'];
+    $receiver = getUserById( $receiver_id );
+    $receiver_nick = $reciever->nickname;
+    $receiver_email = $receiver->username;
+} else if( $who == 'runner' ) {
+    // Get runner Nickname & email
+    $receiver_id = $item['runner_id'];
+    $receiver = getUserById( $receiver_id );
+    $receiver_nick = $receiver->nickname;
+    $receiver_email = $reciever->username;
+}
 
 // Compose journal message
-$out_msg = $nickname." sent a ping to ".$mechanic_nick." about item #".$item_id;
+$out_msg = $nickname." sent a ping to ".$receiver_nick." about item #".$item_id;
 $out_msg .= ": ".$msg;
 
 // Send to journal
@@ -41,11 +51,11 @@ sendJournalNotification( $out_msg );
 // Send mail
 if( $send_mail )	{
 	$mail_subject = $nickname." sent you a ping for item #".$item_id;
-	$mail_msg = "<p>Dear ".$mechanic_nick.",<br/>".$nickname." sent you a ping about item ";
+	$mail_msg = "<p>Dear ".$receiver_nick.",<br/>".$nickname." sent you a ping about item ";
 	$mail_msg .= "<a href='http://dev.sendlove.us/worklist/workitem.php?job_id=".$item_id."&action=view'>#".$item_id."</a>";
 	$mail_msg .= "</p><p>Message:<br/>".$msg."</p><p>You can answer to ".$nickname." at: ".$email."</p><p>LoveMachine</p>";
 
-	sl_send_email( $mechanic_email, $mail_subject, $mail_msg);
+	sl_send_email( $receiver_email, $mail_subject, $mail_msg);
 }
 	
 ?>
