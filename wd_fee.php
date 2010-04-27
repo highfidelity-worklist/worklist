5b7da94611ae209da04754f3d356bc6376751e89
jason <jason@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-04-27 04:17:22 +0000

    #11157 - add file to do fee WD inline

---
+++ wd_fee.php
@@ -0,0 +1,17 @@
+<?php
+
+include("config.php");
+
+//open db connection
+$db = @mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die ('I cannot connect to the database because: ' . mysql_error());
+$db = @mysql_select_db(DB_NAME);
+
+
+$fee_id = $_GET["wd_fee_id"];
+
+$fee_update_sql = 'UPDATE '.FEES.' SET withdrawn = \'1\' WHERE id = '.$fee_id;
+$fee_update = mysql_query($fee_update_sql);
+
+if ($fee_update) { echo 'Update Successful!'; } else { echo 'Update Failed: '.mysql_error(); }
+
+?>
