503d3a02a416739832563fae7a42eb053ac1565f
tom <tom@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-04-28 14:18:30 +0000

    changed WD to DEL and prevent deleteing an already paid fee in worklist detail view (fixed a bug)

--- worklist.php
+++ worklist.php
@@ -845,7 +845,8 @@ include("head.html"); ?>
 			var wd = ' - <form action="" method="post">' +
 							'<input type="hidden" name="withdraw_bid" value="withdraw" />' +
 							'<input type="hidden" name="fee_id" value="' + json[0] + '" />' +
-							'<a href="#" class = "wd-link" title='Delete Entry'>DEL</a>' +
+							'<a href="#" 
+class = "wd-link" title="Delete Entry">DEL</a>' +
 						 '</form>';
 		}
 
