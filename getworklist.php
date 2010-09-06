4f33a0dab3a72c02616908d482c4a14e1d8b31a2
vincent <vincent@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2010-09-06 15:23:46 +0000

    Worklist: Certain jobs not visible from Joblist. #12437

--- getworklist.php
+++ getworklist.php
@@ -200,8 +200,6 @@ if (($ufilter == 'ALL') && ($sfilter[0] == 'BIDDING') && (isset($_SESSION['useri
     $qsel .= ", (SELECT COUNT(`".BIDS."`.`id`) FROM `".BIDS."` WHERE `".BIDS."`.`worklist_id` = `".WORKLIST."`.`id` AND `".BIDS."`.`bidder_id` = ".$_SESSION['userid'].") AS `bid_on`";
 }
 
-// add where clause to not show status-level if bid was withdrawn.
-$where .= " AND (`withdrawn` = 0)";
 
 $qbody = "FROM `".WORKLIST."`
           LEFT JOIN `".USERS."` AS cu ON `".WORKLIST."`.`creator_id` = `cu`.`id`
@@ -211,8 +209,8 @@ $qbody = "FROM `".WORKLIST."`
 		  LEFT OUTER JOIN `".USERS."` AS mu ON `".WORKLIST."`.`mechanic_id` = `mu`.`id`
           LEFT JOIN `tmp_totals` AS `totals` ON `".WORKLIST."`.`id` = `totals`.`worklist_id`
           $unpaid_join
-          LEFT JOIN `tmp_bids` AS `bids` ON `".WORKLIST."`.`id` = `bids`.`worklist_id`
-          $where ";
+          LEFT JOIN `tmp_bids` AS `bids` ON `".WORKLIST."`.`id` = `bids`.`worklist_id` AND (`withdrawn` = 0) 
+          $where ";                                                                         // add  to not show status-level if bid was withdrawn.
 
 $qorder = "ORDER BY {$ofilter} {$dfilter} LIMIT " . ($page-1)*$limit . ",{$limit}";
 
