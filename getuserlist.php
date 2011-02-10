bc177d9d5cdbb4b8e97323dcdf830d29258c431c
andres <andres@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2011-02-10 00:46:33 +0000

    Fixed bug on team page where, if a user had none created jobs or wasn't mechanic on any their job count would be 0 instead of the sum of both. Fixed the way the earnings are fetched so that suggested item fees won't count.

--- getuserlist.php
+++ getuserlist.php
@@ -7,6 +7,7 @@
 include("config.php");
 include("class.session_handler.php");
 require_once('classes/User.class.php');
+include("functions.php");
 
 $limit = 30;
 $page = isset($_REQUEST["page"])?intval($_REQUEST["page"]) : 1;
@@ -45,12 +46,12 @@ $cPages = ceil($users/$limit);
 if( $active == 'FALSE' ) {
     $query = "
     SELECT `id`, `nickname`,DATE_FORMAT(`added`, '%m/%d/%Y') AS `joined`, `annual_salary`, `budget`,
-    IFNULL(`creators`.`count` + `mechanics`.`count`,0) AS `jobs_count`, 
+    IFNULL(`creators`.`count`,0) + IFNULL(`mechanics`.`count`,0) AS `jobs_count`, 
     IFNULL(`earnings`.`sum`,0) AS `earnings`,
     IFNULL(`rewarder`.`sum`,0) AS `rewarder`
     FROM `".USERS."` 
-    LEFT JOIN (SELECT `mechanic_id`, COUNT(`mechanic_id`) AS `count` FROM `".WORKLIST."` GROUP BY `mechanic_id`) AS `mechanics` ON `".USERS."`.`id` = `mechanics`.`mechanic_id` 
-    LEFT JOIN (SELECT `creator_id`, COUNT(`creator_id`) AS `count` FROM `".WORKLIST."` GROUP BY `creator_id`) AS `creators` ON `".USERS."`.`id` = `creators`.`creator_id` 
+    LEFT JOIN (SELECT `mechanic_id`, COUNT(`mechanic_id`) AS `count` FROM `".WORKLIST."` WHERE `status`!='SUGGESTED' GROUP BY `mechanic_id`) AS `mechanics` ON `".USERS."`.`id` = `mechanics`.`mechanic_id` 
+    LEFT JOIN (SELECT `creator_id`, COUNT(`creator_id`) AS `count` FROM `".WORKLIST."` WHERE `status`!='SUGGESTED' GROUP BY `creator_id`) AS `creators` ON `".USERS."`.`id` = `creators`.`creator_id` 
     LEFT JOIN (SELECT `user_id`, SUM(amount) AS `sum` FROM `".FEES."` WHERE $sfilter AND `withdrawn`=0 AND `rewarder`=1 GROUP BY `user_id`) AS `rewarder` ON `".USERS."`.`id` = `rewarder`.`user_id`  
     LEFT JOIN (SELECT `user_id`, SUM(amount) AS `sum` FROM `".FEES."` WHERE $sfilter AND `withdrawn`=0 AND `expense`=0 and `rewarder`=0 GROUP BY `user_id`) AS `earnings` ON `".USERS."`.`id` = `earnings`.`user_id`  
     LEFT JOIN (SELECT `user_id`, SUM(amount) AS `sum` FROM `".FEES."` WHERE $sfilter AND `withdrawn`=0 and `expense`=1 GROUP BY `user_id`) AS `expenses_billed` ON `".USERS."`.`id` = `expenses_billed`.`user_id`  
@@ -58,28 +59,30 @@ if( $active == 'FALSE' ) {
 }    else if( $active == 'TRUE' )    {
     $query = "
     SELECT `id`, `nickname`,DATE_FORMAT(`added`, '%m/%d/%Y') AS `joined`,`annual_salary`, `budget`,
-    IFNULL(`creators`.`count` + `mechanics`.`count`,0) AS `jobs_count`, 
+    IFNULL(`creators`.`count`,0) + IFNULL(`mechanics`.`count`,0) AS `jobs_count`, 
     IFNULL(`earnings`.`sum`,0) AS `earnings`,
     IFNULL(`rewarder`.`sum`,0) AS `rewarder`
     FROM `".USERS."` 
     LEFT JOIN (SELECT `user_id`,MAX(`date`) AS `date` FROM `".FEES."` GROUP BY `user_id`) AS `dates` ON `".USERS."`.id = `dates`.user_id
-    LEFT JOIN (SELECT `mechanic_id`, COUNT(`mechanic_id`) AS `count` FROM `".WORKLIST."` GROUP BY `mechanic_id`) AS `mechanics` ON `".USERS."`.`id` = `mechanics`.`mechanic_id` 
-    LEFT JOIN (SELECT `creator_id`, COUNT(`creator_id`) AS `count` FROM `".WORKLIST."` GROUP BY `creator_id`) AS `creators` ON `".USERS."`.`id` = `creators`.`creator_id` 
+    LEFT JOIN (SELECT `mechanic_id`, COUNT(`mechanic_id`) AS `count` FROM `".WORKLIST."` WHERE `status`!='SUGGESTED' GROUP BY `mechanic_id`) AS `mechanics` ON `".USERS."`.`id` = `mechanics`.`mechanic_id` 
+    LEFT JOIN (SELECT `creator_id`, COUNT(`creator_id`) AS `count` FROM `".WORKLIST."` WHERE `status`!='SUGGESTED' GROUP BY `creator_id`) AS `creators` ON `".USERS."`.`id` = `creators`.`creator_id` 
     LEFT JOIN (SELECT `user_id`, SUM(amount) AS `sum` FROM `".FEES."` WHERE $sfilter AND `withdrawn`=0 AND `rewarder`=1 GROUP BY `user_id`) AS `rewarder` ON `".USERS."`.`id` = `rewarder`.`user_id`  
     LEFT JOIN (SELECT `user_id`, SUM(amount) AS `sum` FROM `".FEES."` WHERE $sfilter AND `withdrawn`=0 AND `expense`=0 AND `rewarder`=0 GROUP BY `user_id`) AS `earnings` ON `".USERS."`.`id` = `earnings`.`user_id`  
     LEFT JOIN (SELECT `user_id`, SUM(amount) AS `sum` FROM `".FEES."` WHERE $sfilter AND `withdrawn`=0 and `expense`=1 GROUP BY `user_id`) AS `expenses_billed` ON `".USERS."`.`id` = `expenses_billed`.`user_id`  
     WHERE `date` > DATE_SUB(NOW(), INTERVAL $sfilter DAY) AND `nickname` REGEXP '^$letter' AND `is_active` = 1  ORDER BY `$order` $order_dir LIMIT " . ($page-1)*$limit . ",$limit";
 }
-
 $rt = mysql_query($query);
 
 // Construct json for pagination
 $userlist = array(array($users, $page, $cPages));
 
 while($row = mysql_fetch_assoc($rt)){
+    $userStats = new UserStats($row['id']);
+    
 	$user = new User();
 	$user->findUserById($row['id']);
 	$row['budget'] = number_format($user->getRemainingFunds(), 0);
+	$row['earnings'] = $userStats->getTotalEarnings();
     $userlist[] = $row;
 }
                       
