ec5530af7659d1708a072b05916a3daa747b6e3a
pedrodias <pedrodias@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2011-03-30 20:59:15 +0000

    #13579

--- functions.php
+++ functions.php
@@ -451,6 +451,49 @@ function payBonusToUser($user_id, $amount, $notes) {
     }
 }
 
+
+function formatableRelativeTime($timestamp, $detailLevel = 1) {
+	$periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+	$lengths = array("60", "60", "24", "7", "4.357", "12", "10");
+	$now = time();
+	if(empty($timestamp)) {
+		return "Unknown time";
+	}
+	if($now > $timestamp) {
+		$difference = $now - $timestamp;
+		$tense = "";
+	} else {
+		$difference = $timestamp - $now;
+		$tense = "from now";
+	}
+	if ($difference == 0) {
+		return "1 second ago";
+	}
+	$remainders = array();
+	for($j = 0; $j < count($lengths); $j++) {
+		$remainders[$j] = floor(fmod($difference, $lengths[$j]));
+		$difference = floor($difference / $lengths[$j]);
+	}
+	$difference = round($difference);
+	$remainders[] = $difference;
+	$string = "";
+	for ($i = count($remainders) - 1; $i >= 0; $i--) {
+		if ($remainders[$i]) {
+			$string .= $remainders[$i] . " " . $periods[$i];
+			if($remainders[$i] != 1) {
+				$string .= "s";
+			}
+			$string .= " ";
+			$detailLevel--;
+			if ($detailLevel <= 0) {
+				break;
+			}
+		}
+	}
+	return $string . $tense;
+}
+
+
 function relativeTime($time) {
     $secs = abs($time);
     $mins = 60;
