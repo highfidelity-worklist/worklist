6c4ca5aee11b3d51cd000401433f885bde0e9f8c
ncat <ncat@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2011-04-16 00:02:50 +0000

    #13736 Fix fatal error from getskills.php

--- getskills.php
+++ getskills.php
@@ -7,8 +7,8 @@ ob_start();
 
 include("config.php");
 include("class.session_handler.php");
-include("check_session.php");
 include("functions.php");
+include("check_session.php");
 
 $con=mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD);
 mysql_select_db(DB_NAME,$con);
