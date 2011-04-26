db1c9e1c4368dd9d8e19d65ba91f7583d42fe627
ncat <ncat@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2011-04-26 13:47:53 +0000

    #13925 worklist.php dialog cleanup

--- worklist.php
+++ worklist.php
@@ -167,7 +167,6 @@ if (!empty($_POST)) {
 $worklist_id = isset($_REQUEST['job_id']) ? intval($_REQUEST['job_id']) : 0;
 
 include("head.html"); ?>
-
 <!-- Add page-specific scripts and styles here, see head.html for global scripts and styles  -->
 <link href="css/worklist.css" rel="stylesheet" type="text/css">
 <link href="css/ui.toaster.css" rel="stylesheet" type="text/css">
@@ -208,7 +207,7 @@ include("head.html"); ?>
     var sort = '<?php echo $filter->getSort(); ?>';
     var inProject = '<?php echo is_object($inProject) ?  $project_id  : '';?>';
     var resetOrder = false;
-        var worklistUrl = '<?php echo SERVER_URL; ?>';
+    var worklistUrl = '<?php echo SERVER_URL; ?>';
     stats.setUserId(user_id);
     var activeProjectsFlag = true;
     var skills = null;
@@ -427,6 +426,7 @@ include("head.html"); ?>
         $('#modalIframeId').attr('src','userinfo.php?id=' + userId);
         return false;
     };
+
     function GetWorklist(npage, update, reload) {
         loaderImg.show("loadRunning", "Loading, please wait ...");
         $.ajax({
@@ -591,265 +591,6 @@ include("head.html"); ?>
     }
 
 
-//Most of the js code for implementing bidding capability starts here
-    function GetBidlist(worklist_id, npage) {
-        $("#bid").unbind( "click" );
-        $.ajax({
-            type: "POST",
-            url: 'getbidlist.php',
-            data: 'worklist_id=' + worklist_id + '&page=' + npage,
-            dataType: 'json',
-            success: function(json) {
-                page = json[0][1]|0;
-                var cPages = json[0][2]|0;
-
-                $('.row-bidlist-live').remove();
-                biditems = json;
-                if (!json[1]){
-                    var row = '<tr bgcolor="#FFFFFF" class="row-bidlist-live bidlist-pagination-row" ><td colspan="4" style="text-align:center;">No bids yet.</td></tr>';
-                    $('.table-bidlist tbody').append(row);
-                    return;
-                }
-
-                var already_bid = false;
-
-                /* Output the bidlist rows. */
-                var odd = topIsOdd;
-                for (var i = 1; i < json.length; i++) {     
-//                  if (json[i].bidder_id == "<?php echo (isset($_SESSION['userid'])) ? $_SESSION['userid'] : ''; ?>"){
-                    if (json[i].bidder_id == "<?php echo getSessionUserId(); ?>"){
-                        already_bid = true;
-                    }
-                    AppendBidRow(json[i], odd);
-                    odd = !odd;
-                }
-
-                AppendPagination(page, cPages, 'bidlist');
-
-                $('.bidlist-pagination-row a').click(function(e){
-                    page = $(this).attr('href').match(/page=\d+/)[0].substr(5);
-                    GetBidlist(worklist_id, page);
-                    e.stopPropagation();
-                    return false;
-                });
-
-                //show additional popup with bid info
-                $('tr.row-bidlist-live').click(function(){
-                    var match = $(this).attr('class').match(/biditem-\d+/);
-                    var bid_id = match[0].substr(8);
-                    ResetBidInfoPopup();
-
-                    AjaxPopup('#popup-bid-info',
-                            'Bid Info',
-                            'getbiditem.php',
-                            bid_id,
-                            [ ['input', 'bid_id', 'keyId', 'eval'],
-                              ['input', 'info-email2', 'json.email', 'eval'],
-                              ['span', '#info-email', 'json.email', 'eval'],
-                              ['span', '#info-bid-created', 'json.bid_created', 'eval'],
-                              ['span', '#info-bid-amount', 'json.bid_amount', 'eval'],
-                              ['span', '#info-bid-done-by', 'json.done_by', 'eval'],
-                              ['span', '#info-notes', 'json.notes', 'eval'] ],
-                            function(json) {
-//                              if (is_runner==1 || runner_id == "<?php echo (isset($_SESSION['userid'])) ? $_SESSION['userid'] : ''; ?>")
-                                if (is_runner==1 || runner_id == "<?php echo getSessionUserId(); ?>")
-
-                                    $('#popup-bid-info form').append('<input type="submit" name="accept_bid" value="Accept">');
-//                              if (is_runner==1 || (json.bidder_id == "<?php echo (isset($_SESSION['userid'])) ? $_SESSION['userid'] : ''; ?>"))
-                                if (is_runner==1 || (json.bidder_id == "<?php echo getSessionUserId(); ?>"))
-                                    $('#popup-bid-info form').append('<input type="submit" name="withdraw_bid" value="Withdraw" style="float:right;">');
-                            });
-
-                $('#popup-bid-info').dialog('open');
-            });
-
-            if(already_bid) {
-                $('#bid').click(function(e){
-                    if (!confirm("You have already placed a bid, do you want to place a new one?")) {
-                        $('#popup-bid').dialog('close');
-                        return false;
-                    }
-                });
-            }
-
-            },
-            error: function(xhdr, status, err) {
-                $('.row-bidlist-live').remove();
-                $('.table-bidlist').append('<tr class="row-bidlist-live rowodd"><td colspan="4" align="center">Oops! We couldn\'t find any bid items.  <a id="againbid" href="#">Please try again.</a></td></tr>');
-                $('#againbid').click(function(e){
-                    if (timeoutId) clearTimeout(timeoutId);
-                    GetBidlist(worklist_id, page);
-                    e.stopPropagation();
-                    return false;
-                });
-            }
-        });
-    }
-
-    //json row
-    //id, bidder_id,email,nickname,worklist_id,bid_amount,bid_created,bid_done,notes,delta
-    function AppendBidRow(json, odd)    {
-        var pre = '', post = '';
-        var row;
-        row = '<tr class="row-bidlist-live ';
-        if (odd) { row += 'rowodd' } else { row += 'roweven' }
-        row += ' biditem-' + json.id + '">'; //id
-        row += '<td width="30%">' + pre + json.email + post + '</td>';//email
-        row += '<td width="20%">' + pre + json.bid_amount + post + '</td>';
-        row += '<td width="20%">' + pre + RelativeTime(json.future_delta) + post + '</td>';
-
-        row += '<td width="20%">' + pre + RelativeTime(json.delta) + ' ago' + post + '</td></tr>';
-       $('.table-bidlist tbody').append(row);
-    }
-
-    function ResetBidInfoPopup(){
-      $('#popup-bid-info form input[type="submit"]').remove();
-    }
-//end of code for bidding table
-
-
-//code for fees table
-    // !!!
-    // !!! This code is duplicated in workitem.inc
-    // !!!
-    function GetFeelist(worklist_id) {
-        $.ajax({
-            type: "POST",
-            url: 'getfeelist.php',
-            data: 'worklist_id=' + worklist_id,
-            dataType: 'json',
-            success: function(json) {
-                $('.row-feelist-live').remove();
-                feeitems = json;
-                if (!json[1])   {
-                  var row = '<tr bgcolor="#FFFFFF" class="row-feelist-live feelist-total-row" >'+
-                            '<td colspan="5" style="text-align:center;">No fees yet.</td></tr>';
-                  $('.table-feelist tbody').append(row);
-                  return;
-                }
-
-                /* Output the bidlist rows. */
-                var odd = topIsOdd;
-                for (var i = 1; i < json.length; i++) {
-                    AppendFeeRow(json[i], odd);
-                    odd = !odd;
-                }
-
-                //will row with total here
-                var row = '<tr bgcolor="#FFFFFF" class="row-feelist-live feelist-total-row" >'+
-                            '<td colspan="5" style="text-align:center;">Total Fees $' + json[0][0] + '</td></tr>';
-                $('.table-feelist tbody').append(row);
-
-                $('.paid-link').click(function(e){
-                    var fee_id = $(this).attr('id').substr(8);
-
-                    AjaxPopup('#popup-paid',
-                          'Pay Fee',
-                          'getfeeitem.php',
-                          fee_id,
-                          [ ['input', 'itemid', 'keyId', 'eval'],
-                            ['textarea', 'paid_notes', 'json[2]', 'eval'],
-                            ['checkbox', 'paid_check', 'json[1]', 'eval'] ]);
-
-                    $('.paidnotice').empty();
-                    $('#popup-paid').dialog('open');
-
-                    // onSubmit event handler for the form
-                    $('#popup-paid > form').submit(function() {
-                        // now we save the payment via ajax
-                        $.ajax({
-                            type: 'POST',
-                            url: 'paycheck.php',
-                            dataType: 'json',
-                            data: {
-                                itemid: $('#' + this.id + ' input[name=itemid]').val(),
-                                paid_check: $('#' + this.id + ' input[name=paid_check]').val(),
-                                paid_notes: $('#' + this.id + ' textarea[name=paid_notes]').val()
-                            },
-                            success: function(data) {
-                                // We need to empty the notice field before we refill it
-                                $('.paidnotice').empty();
-                                if (!data.success) {
-                                    // Failure message
-                                    var html = '<div style="padding: 0 0.7em; margin: 0.7em 0;" class="ui-state-error ui-corner-all">' +
-                                                '<p><span style="float: left; margin-right: 0.3em;" class="ui-icon ui-icon-alert"></span>' +
-                                                '<strong>Alert:</strong> ' + data.message + '</p>' +
-                                                '</div>';
-                                    $('.paidnotice').append(html);
-                                    // Fire the failure event
-                                    $('#popup-paid > form').trigger('failure');
-                                } else {
-                                    // Success message
-                                    var html = '<div style="padding: 0 0.7em; margin: 0.7em 0;" class="ui-state-highlight ui-corner-all">' +
-                                                    '<p><span style="float: left; margin-right: 0.3em;" class="ui-icon ui-icon-info"></span>' +
-                                                    '<strong>Info:</strong> ' + data.message + '</p>' +
-                                                '</div>';
-                                    $('.paidnotice').append(html);
-                                    // Fire the success event
-                                    $('#popup-paid > form').trigger('success');
-                                }
-                            }
-                        });
-
-                        return false;
-                    });
-
-                    // Here we need to capture the event and fire a new one to the upper container
-                    $('#popup-paid > form').bind('success', function(e, d) {
-                        $('.table-feelist tbody').empty();
-                        GetFeelist(worklist_id);
-                    });
-
-                    return false;
-                });
-
-                $('.wd-link').click(function(e) {
-                    $(this).parent().submit();
-                });
-            },
-            error: function(xhdr, status, err) {
-                $('.row-feelist-live').remove();
-                $('.table-feelist').append('<tr class="row-feelist-live rowodd"><td colspan="5" align="center">Oops! We couldn\'t find any fees.  <a id="againfee" href="#">Please try again.</a></td></tr>');
-                $('#againfee').click(function(e){
-                    GetFeelist(worklist_id);
-                    e.stopPropagation();
-                    return false;
-                });
-            }
-        });
-    }
-
-    function AppendFeeRow(json, odd)    {
-        var pre = '', post = '';
-        var row;
-        row = '<tr class="row-feelist-live "';
-        if (odd) { row += 'rowodd' } else { row += 'roweven' }
-        row += ' feeitem-' + json[0] + '">';
-        row += '<td>' + pre + json[2] + post + '</td>';
-        row += '<td>' + pre + json[1] + post + '</td>';
-        row += '<td>' + pre + json[3] + post + '</td>';
-        row += '<td>' + pre + json[4] + post + '</td>';
-
-        var paid = (json[5] == 0) ? 'No' : 'Yes';
-        if(is_payer) {
-            pre = '<a href="#" class = "paid-link" id = "feeitem-' + json[0] + '" >';
-            post = '</a>';
-        }
-
-        var wd = '';
-        if (is_runner) {
-            var wd = ' - <form action="" method="post">' +
-                            '<input type="hidden" name="withdraw_bid" value="withdraw" />' +
-                            '<input type="hidden" name="fee_id" value="' + json[0] + '" />' +
-                            '<a href="#" class = "wd-link" title="Delete Entry">DEL</a>' +
-                         '</form>';
-        }
-
-        row += '<td>' + pre + paid + post + wd + '</td></tr>';
-        $('.table-feelist tbody').append(row);
-    }
-
-//end of code for fees table
 
     jQuery.fn.center = function () {
       this.css("position","absolute");
@@ -1025,27 +766,7 @@ include("head.html"); ?>
                 }
             }
         });
-        $('#popup-bid').dialog({ autoOpen: false, maxWidth: 600, width: 450, show: 'fade', hide: 'fade'});
-        $('#popup-bid-info').dialog({ autoOpen: false, modal: true, show: 'fade', hide: 'fade'});
-        $('#popup-addfee').dialog({ autoOpen: false, modal: true, width: 400, show: 'fade', hide: 'fade'});
-        $('#popup-paid').dialog({ autoOpen: false, maxWidth: 600, width: 450, show: 'fade', hide: 'fade'});
-        $('#pages-dialog').dialog({ autoOpen: false });
         $('#budget-expanded').dialog({ autoOpen: false, width:920, show:'fade', hide:'drop' });
-
-        $('#done_by').datepicker({
-            duration: '',
-            showTime: false,
-            constrainInput: false,
-            stepMinutes: 1,
-            stepHours: 1,
-            altTimeField: '',
-            time24h: false
-        });
-
-        $('#popup-bid').bind('dialogclose', function(){
-            $('#ui-datepicker-div').hide();
-            $('#ui-timepicker-div').hide();
-        });
         $('#user-info').dialog({
            autoOpen: false,
            resizable: false,
@@ -1201,29 +922,6 @@ include("head.html"); ?>
             $('#fees_single_block').show();
             $('#popup-edit').dialog('open');
         });
-
-        $('.popup-body form input[type="submit"]').click(function(){
-            var name = $(this).attr('name');
-            $(".popup-page-value").val(page);
-
-            switch(name)    {
-                case "add_fee_dialog":
-                    SimplePopup('#popup-addfee',
-                                'Add Fee',
-                                workitem,
-                                [['input', 'itemid', 'keyId', 'eval']]);
-                    $('#popup-addfee').dialog('open');
-                    return false;
-                case "reset":
-                    ResetPopup();
-                    return false;
-                case "cancel":
-                    $('#popup-edit').dialog('close');
-                    $('#popup-paid').dialog('close');
-                    return false;
-            }
-        });
-
         $("#search").click(function(e){
             e.preventDefault();
             $("#searchForm").submit();
@@ -1254,20 +952,6 @@ include("head.html"); ?>
             return false;
         });
 
-        $('#page-go').click(function(){
-            var npage = $('#pages-dialog select').val();
-            if(npage != page){
-                getIdFromPage(npage, $('#pages-dialog #worklist-id').val(), 0);
-            }
-            $('#pages-dialog').dialog('close');
-            return false;
-        });
-    
-        $('#page-go-highest').click(function()  {
-            updatePriority($('#pages-dialog #worklist-id').val(), 0, 5);
-            $('#pages-dialog').dialog('close');
-            return false;
-        });
         //-- gets every element who has .iToolTip and sets it's title to values from tooltip.php
         /* function commented for remove tooltip */
         //setTimeout(MapToolTips, 800);
@@ -1676,34 +1360,14 @@ var documentsArray = new Array();
 
 <!-- Popup for editing/adding  a work item -->
 <?php require_once('dialogs/popup-edit.inc'); ?>
-
-<!-- Popup HTML for paying a fee -->
-<?php require_once('dialogs/popup-paid-html.inc'); ?>
-
-<!-- Popup for placing a bid -->
-<?php require_once('dialogs/popup-bid.inc'); ?>
-
-<!-- Popup for bid info-->
-<?php require_once('dialogs/popup-bid-info.inc'); ?>
-
-<!-- Popup for adding fee-->
-<?php require_once('dialogs/popup-addfee.inc'); ?>
-
 <!-- Popup for breakdown of fees-->
 <?php require_once('dialogs/popup-fees.inc'); ?>
-
 <!-- Popup for budget info -->
 <?php require_once('dialogs/budget-expanded.inc') ?>
-
-<!-- Div for moving items accross the pages -->
-<?php require_once('dialogs/pages-dialog.inc'); ?>
-
 <!-- Popups for tables with jobs from quick links -->
 <?php require_once('dialogs/popups-userstats.inc'); ?>
-
 <!-- Popup for add project info-->
 <?php require_once('dialogs/popup-addproject.inc'); ?>
-
 <?php include("format.php"); ?>
 <!-- ---------------------- BEGIN MAIN CONTENT HERE ---------------------- -->
 
