c657aeff574e02e4e1b156d7a6f7b169fd02462b
mikewasmike <mikewasmike@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2012-01-18 18:45:02 +0000

    #16419 Add a Reply to Bid option for Runners

--- pingtask.php
+++ pingtask.php
@@ -31,7 +31,7 @@ $send_cc = isset($_REQUEST['cc']) ? (int) $_REQUEST['cc'] : false;
 if (isset($_REQUEST['id'])) {
     $item_id = intval($_REQUEST['id']);
     $who = $_REQUEST['who'];
-
+    $bid_id = intval($_REQUEST['bid_id']);
     // Get item
     $item = getWorklistById( $item_id );
 
@@ -53,6 +53,15 @@ if (isset($_REQUEST['id'])) {
         $receiver = getUserById( $receiver_id );
         $receiver_nick = $receiver->nickname;
         $receiver_email = $receiver->username;
+    } else if ($who == 'bidder') {
+        // Get bidder Nickname & email
+        $bid = new Bid();
+        $bid->findBidById($bid_id);
+        $bid_info = $bid->toArray();
+        $receiver_id = $bid_info['bidder_id'];
+        $receiver = getUserById( $receiver_id );
+        $receiver_nick = $receiver->nickname;
+        $receiver_email = $receiver->username;
     }
 
     // Compose journal message
@@ -65,12 +74,38 @@ if (isset($_REQUEST['id'])) {
     }
 
     // Send mail
-    if ($send_mail) {
+    if ($send_mail && $who != 'bidder') {
         $mail_subject = $nickname." sent you a ping for item #".$item_id;
         $mail_msg = "<p>Dear ".$receiver_nick.",<br/>".$nickname." sent you a ping about item ";
         $mail_msg .= "<a href='".WORKLIST_URL."/workitem.php?job_id=".$item_id."&action=view'>#".$item_id."</a>";
         $mail_msg .= "</p><p>Message:<br/>".$msg."</p><p>You can answer to ".$nickname." at: ".$email."</p>";
         $headers = array('X-tag' => 'ping, task', 'Reply-To' => '"' . $nickname . '" <' . $email . '>');
+        if ($send_cc) {
+            $headers['Cc'] = '"' . $nickname . '" <' . $email . '>';
+        }
+        if (!send_email($receiver_email, $mail_subject, $mail_msg, '', $headers)) { 
+            error_log('pingtask.php:id: send_email failed');
+        }
+
+        // sms
+        $user = new User();
+        $user->findUserById($receiver->id);
+        if (Notification::isNotified($user->getNotifications(), Notification::PING_NOTIFICATIONS)) {
+            notify_sms_by_object($user, $mail_subject, $msg);
+        }
+    } else if ($send_mail && $who == 'bidder') {
+        $project = new Project();
+        $project->loadById($item['project_id']);
+        $project_name = $project->getName();
+        $mail_subject = "#" . $item_id . " - " . $item['summary'];
+        $mail_msg = "<p>The Runner for #" . $item_id . " sent a reply to your bid.</p>";
+        $mail_msg .= "<p>Message from " . $nickname . ":<br/>" . $msg . "</p>";
+        $mail_msg .= "<p>Your bid info:</p>";
+        $mail_msg .= "<p>Amount: " . $bid_info['bid_amount'] . "<br />Done in: " . $bid_info['bid_done_in'] . "<br />Expires: " . $bid_info['bid_expires'] . "</p>";
+        $mail_msg .= "<p>Notes: " . $bid_info['notes'] . "</p>";
+        $mail_msg .= "<p>You can view the job here. <a href='".WORKLIST_URL."/workitem.php?job_id=" . $item_id . "&action=view'>#" . $item_id . "</a></p>";
+        $mail_msg .= "<p>-Worklist.net</p>";
+        $headers = array('From' => '"'. $project_name.'-bid reply" <'. SMS_SENDER . '>', 'X-tag' => 'ping, task', 'Reply-To' => '"' . $nickname . '" <' . $email . '>');
         if ($send_cc) {
             $headers['Cc'] = '"' . $nickname . '" <' . $email . '>';
         }
