6098db5f5253cbcc38be985d4337e97a077688f8
randomecho <randomecho@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2012-07-04 14:11:38 +0000

    #17526 - fallback to large site logo if project logo too small

--- opengraphmeta.php
+++ opengraphmeta.php
@@ -11,12 +11,20 @@ if(is_object($inProject))
 {
 	$meta_title = 'Worklist Project: ' . $inProject->getName();
 	$meta_desc = $inProject->getDescription();
-	$meta_image = ($inProject->getLogo()) ? 'uploads/'.$inProject->getLogo() : $meta_image;
-}
+	$min_dimensions = 120; // Minimum image dimension some social networks must have before showing thumb
 
+	if($inProject->getLogo())
+	{
+		list($check_width, $check_height) = getimagesize(UPLOAD_PATH.$inProject->getLogo());
+
+		if($check_width >= $min_dimensions && $check_height >= $min_dimensions)
+		{	// Override default if image is large enough
+			$meta_image = 'uploads/'.$inProject->getLogo();
+		}
+	}
+}
 ?>
 <meta property="og:title" content="<?php echo $meta_title; ?>" />
 <meta name="description" content="<?php echo $meta_desc; ?>" />
 <meta property="og:description" content="<?php echo $meta_desc; ?>" />
 <meta property="og:image" content="<?php echo SERVER_URL . $meta_image; ?>" />
-<link rel="image_src" content="<?php echo SERVER_URL . $meta_image; ?>" />
