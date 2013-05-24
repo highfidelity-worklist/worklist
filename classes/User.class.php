73a930748ce32ebaaf10c632f23e63c6d74937c0
leo <leo@2200db0b-cb50-4961-a5eb-5d1a25769d08>
2013-05-24 21:48:29 +0000

    Cleaning up fix

--- classes/User.class.php
+++ classes/User.class.php
@@ -1575,11 +1575,8 @@ class User {
         if (empty($this->picture)) {
             return SERVER_URL ."thumb.php?src=no_picture.png&h=".$h."&w=".$w."&zc=0";
         } else {
-        // if ($this->imageExistsS3($this->picture)) {
             return APP_IMAGE_URL . $this->picture;
-        } //else {
-        //    return SERVER_URL ."thumb.php?src=".$this->picture."&h=".$h."&w=".$w."&zc=0";
-        //}
+        }
     }
     
     /**
