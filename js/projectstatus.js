8b483deaffcb1ba8c294a855ba199a29745ca835
Lucas Crisman <kordero@gmail.com>
2014-02-22 22:01:51 +0000

    adding projectstatus js

---
+++ js/projectstatus.js
@@ -0,0 +1,11 @@
+$(function() {
+    if (!errorOut) {
+        WorklistProject.repo_type = repo_type;
+        WorklistProject.init();
+        if (!GitHub.validate()) {
+            GitHub.handleUserConnect();
+        }        
+    } else {
+        $('#project-status').html('<h3>Something went wrong! <?php echo $errorOut; ?></h3>');
+    }
+});
