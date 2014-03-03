43363fd45da9d65cb2db3ecd426673aa95c4ca5e
Lucas Crisman <kordero@gmail.com>
2014-03-03 11:00:08 +0000

    Welcome page changes

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -2,7 +2,8 @@
 
 class WelcomeController extends Controller {
     public function run() {
-        Session::check();
+        $stats = getStats('Bidding');
+        $this->write('biddingJobs', array_slice($stats, 0, 5));
         parent::run();
     }
 }
--- views/Welcome.php
+++ views/Welcome.php
@@ -6,4 +6,27 @@ class WelcomeView extends View {
     public $stylesheets = array(
         'css/welcome.css'
     );
+
+    public function areBiddingJobs() {
+        return count($this->read('biddingJobs')) > 0;
+    }
+
+    public function biddingJobs() {
+        $jobs = $this->read('biddingJobs');
+        $ret = array();
+        foreach($jobs as $key => $job_number) {
+            $workitem = WorkItem::getById($job_number);
+            if (!$workitem->getProjectId()) {
+                continue;
+            }
+            $project = Project::getById($workitem->getProjectId());
+            $ret[] = array(
+                'id' => $workitem->getId(),
+                'summary' => $workitem->getSummary(),
+                'skills' => implode(', ', $workitem->getSkills()),
+                'project' => $project->getName()
+            );
+        }
+        return $ret;
+    }
 }
