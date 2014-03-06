d9b3871bef1ed73ef4e4b25223be47d1abe84640
Lucas Crisman <kordero@gmail.com>
2014-03-06 19:53:02 -0300

    status: feed pull requests from highfideilty/hifi

--- controllers/Status.php
+++ controllers/Status.php
@@ -1,9 +1,30 @@
 <?php
 
 class StatusController extends Controller {
+    protected $token = GITHUB_API_TOKEN;
+    protected $client = null;
+
     public function run() {
+        $this->client = new Github\Client(
+            new Github\HttpClient\CachedHttpClient(array(
+                'cache_dir' => TEMP_DIR . DIRECTORY_SEPARATOR . 'github'
+            ))
+        );
+        $this->client->authenticate($this->token, '', Github\Client::AUTH_HTTP_TOKEN);
+        $this->write('pullreqs', $this->listRepoPullRequests(GITHUB_REPO));
+
         $entry = new EntryModel();
-        $this->write('entries', $entry->latest(60 * 24 * 30, 200));
+        $this->write('entries', $entry->latest(60 * 24 * 30, 50));
         parent::run();
+    }
+
+    protected function listRepoPullRequests($repo, $state = 'all') {
+        try {
+            list($owner, $repo) = explode('/', $repo);
+            $pullreqs = $this->client->api('pull_request')->all($owner, $repo, $state);
+            return $pullreqs;
+        } catch(Exception $e) {
+            return false;
+        }
     }
 }
--- views/Status.php
+++ views/Status.php
@@ -23,6 +23,54 @@ class StatusView extends View {
     }
 
     public function entries() {
-        return array_reverse($this->read('entries'));
+        $worklist_entries = array_reverse($this->read('entries'));
+        $gh_pullreqs = $this->read('pullreqs');
+        $entries = array_merge($worklist_entries, $gh_pullreqs);
+        usort($entries, array('StatusView', 'sortEntries'));
+        $ret = '';
+        $now = 0;
+
+        foreach($entries as $entry) {
+            if (!$now) {
+                $now = strtotime(Model::now());
+            }
+            if (get_class($entry) == 'EntryModel') {
+                $id = $entry->id;
+                $source = 'worklist';
+                $class = $entry->oldFormatEntry ? '' : 'markdown';
+                $date = $entry->date;
+                $content = $entry->oldFormatEntry() ? $entry->entry : $entry->markdownEntry();
+            } else { // github pull request
+                $id = $entry['sha'];
+                $source = 'github-pullreq';
+                $class = '';
+                $date = $entry['updated_at'];
+                $content = $entry['state'] . ' - ' . $entry['title'];
+            }
+            $time = strtotime($date);
+
+            $ret .= 
+                  '<li entryid="' . $id . '" date="' . $date  . '" source="' . $source .  '" class="' . $class . '">'
+                .     '<h4>' . relativeTime($time - $now) . '</h4>'
+                .     $content
+                . '</li>';
+        }
+        return $ret;
+    }
+
+    static function sortEntries($a, $b) {
+        if (get_class($a) == 'EntryModel') {
+            $date_a = $a->date;
+        } else { // pull request
+            $date_a = $a['updated_at'];
+        }
+        if (get_class($b) == 'EntryModel') {
+            $date_b = $b->date;
+        } else { // pull request
+            $date_b = $b['updated_at'];
+        }
+        $time_a = strtotime($date_a);
+        $time_b = strtotime($date_b);
+        return ($time_a == $time_b) ? 0 : ($time_a > $time_b ? +1 : -1);
     }
 }
