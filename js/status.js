9183d8669134ca8da750aafe8f03b98b975fe23d
Lucas Crisman <kordero@gmail.com>
2014-03-07 18:29:24 -0300

    status page: color stling worklist entries

--- js/status.js
+++ js/status.js
@@ -1,8 +1,21 @@
 $(function() {
     // Collect Bidding Jobs info
-    getBiddingReviewDrawers();
-    
+    getBiddingReviewDrawers();    
     window.scrollTo(0, $('#entries').outerHeight());
+
+    $('#entries > li[type="worklist"]').each(function() {
+        var text = '';
+        $('p', $(this)).each(function(){
+            text += text ? '. ' : '';
+            text += $(this).text();
+        });
+
+        var updated_regex = /\d+\s(?:updated|created)\sby\s\w+.+Status\sset\sto\s+(\w+)\.?\s*$/i;
+        var ret = updated_regex.exec(text);
+        if (ret && ret[1]) {
+            $(this).addClass('status' + ret[1]);
+        }
+    });
 });
 
 
