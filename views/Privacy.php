289f79dfbd07c932a4cd9dfb09beb9b2ba30abb9
Lucas Crisman <kordero@gmail.com>
2014-03-13 16:28:46 -0300

    Privacy page to NewWorklist layout

--- views/Privacy.php
+++ views/Privacy.php
@@ -1,9 +1,7 @@
 <?php
 
 class PrivacyView extends View {
+    public $layout = 'NewWorklist';
     public $title = 'Privacy Statement - Worklist';
-
-    public $stylesheets = array(
-        'css/worklist.css'
-    );
+    public $stylesheets = array('css/privacy.css');
 }
