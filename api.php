c75edeb7d1a6403602a47f3cface3998193606bc
Lucas Crisman <kordero@gmail.com>
2014-03-19 01:09:09 -0300

    fix for blank status issue when adding a job as normal user

--- api.php
+++ api.php
@@ -1187,131 +1187,122 @@ function addWorkitem() {
     $workitem = new WorkItem();
 
     $userId = getSessionUserId();
-    if ($userId > 0) {
-        initUserById($userId);
-        $user = new User();
-        $user->findUserById( $userId );
-        $nick = $user->getNickname();
-    }
-
-    if ($userId > 0 ) {
-        $args = array(
-            'itemid',
-            'summary',
-            'project_id',
-            'skills',
-            'status',
-            'notes',
-            'invite',
-            'is_expense',
-            'is_rewarder',
-            'is_bug',
-            'bug_job_id',
-            'fileUpload'
-        );
-
-        foreach ($args as $arg) {
-            // Removed mysql_real_escape_string, because we should 
-            // use it in sql queries, not here. Otherwise it can be applied twice sometimes
-            $$arg = !empty($_REQUEST[$arg])?$_REQUEST[$arg]:'';
-        }
-
-
-        $creator_id = $userId;
-
-        if (! empty($_POST['itemid'])) {
-            $workitem->loadById($_POST['itemid']);
-        } else {
-            $workitem->setCreatorId($creator_id);
-            $workitem_added = true;
-        }
-        $workitem->setSummary($summary);
-
-        //If this item is a bug add original item id 
-        $workitem->setBugJobId($bug_job_id);
-        // not every runner might want to be assigned to the item he created - only if he sets status to 'BIDDING'
-        if ($status == 'Bidding' && $user->getIs_runner() == 1) {
-            $runner_id = $userId;
-        } else {
-            $runner_id = 0;
-        }
-
-        $skillsArr = explode(', ', $skills);
-
-        $workitem->setRunnerId($runner_id);
-        $workitem->setProjectId($project_id);
-        $workitem->setStatus($status);
-        $workitem->setNotes($notes);
-        $workitem->setWorkitemSkills($skillsArr);
-        $workitem->setIs_bug($is_bug == 'true' ? 1 : 0);
-        $workitem->save();
-        $related = getRelated($notes);
-        Notification::massStatusNotify($workitem);
-
-        // if files were uploaded, update their workitem id
-        $file = new File();
-        // update images first
-        if (isset($fileUpload['images'])) {
-            foreach ($fileUpload['images'] as $image) {
-                $file->findFileById($image);
-                $file->setWorkitem($workitem->getId());
-                $file->save();
-            }
-        }
-        // update documents
-        if (isset($fileUpload['documents'])) {
-            foreach ($fileUpload['documents'] as $document) {
-                $file->findFileById($document);
-                $file->setWorkitem($workitem->getId());
-                $file->save();
-            }
-        }
-
-        if($is_bug && $bug_job_id > 0) {
-            //Load information about original job and notify
-            //users with fees and runner
-            Notification::workitemNotify(array(
-                'type' => 'bug_found',
-                'workitem' => $workitem,
-                'recipients' => array('runner', 'usersWithFeesBug')
-            ));
-            Notification::workitemSMSNotify(array(
-                'type' => 'bug_found',
-                'workitem' => $workitem,
-                'recipients' => array(
-                    'runner', 
-                    'usersWithFeesBug'
-                )
-            ));
-            $bugJournalMessage= " (bug of **#" . $workitem->getBugJobId() ."**)";
-        } else {
-            $bugJournalMessage= "";
-        }
-        
-        if (empty($_POST['itemid'])) {
-            $bid_fee_itemid = $workitem->getId();
-            $journal_message .= '**#' . $bid_fee_itemid . '** created by @' . $nick . ' ' . $bugJournalMessage;
-            $journal_message .= "\n\n**" . $summary . '**. Status set to *' . $status . '*';
-            if (!empty($_POST['files'])) {
-                $files = explode(',', $_POST['files']);
-                foreach ($files as $file) {
-                    $sql = 'UPDATE `' . FILES . '` SET `workitem` = ' . $bid_fee_itemid . ' WHERE `id` = ' . (int)$file;
-                    mysql_query($sql);
-                }
-            }
-        } else {
-            $bid_fee_itemid = $itemid;
-            $journal_message .= '#' . $bid_fee_itemid . ' updated by @' . $nick ;
-            $journal_message .=  "\n\n**" . $summary . '**. Status set to *' . $status . '*';
-        }
-        $journal_message .=  "$related. ";
-        if (!empty($_POST['invite'])) {
-            $people = explode(',', $_POST['invite']);
-            invitePeople($people, $workitem);
-        }
-    } else {
+    if (!$userId > 0 ) {
         echo json_encode(array('error' => "Invalid parameters !"));
         return;
+    }
+
+    initUserById($userId);
+    $user = new User();
+    $user->findUserById( $userId );
+    $nick = $user->getNickname();
+
+    $itemid = $_REQUEST['itemid'];
+    $summary = $_REQUEST['summary'];
+    $project_id = $_REQUEST['project_id'];
+    $skills = $_REQUEST['skills'];
+    $status = $_REQUEST['status'];
+    $notes = $_REQUEST['notes'];
+    $invite = $_REQUEST['invite'];
+    $is_expense = $_REQUEST['is_expense'];
+    $is_rewarder = $_REQUEST['is_rewarder'];
+    $is_bug = $_REQUEST['is_bug'];
+    $bug_job_id = $_REQUEST['bug_job_id'];
+    $fileUpload = $_REQUEST['fileUpload'];
+
+    if (!$user->getIs_runner() || !preg_match('/^(Draft|Suggested|Bidding|Working|Done)$/', $status)) {
+        $status = 'Suggested';
+    }
+
+    if (! empty($_POST['itemid'])) {
+        $workitem->loadById($_POST['itemid']);
+    } else {
+        $workitem->setCreatorId($userId);
+        $workitem_added = true;
+    }
+    $workitem->setSummary($summary);
+
+    //If this item is a bug add original item id 
+    $workitem->setBugJobId($bug_job_id);
+    // not every runner might want to be assigned to the item he created - only if he sets status to 'BIDDING'
+    if ($status == 'Bidding' && $user->getIs_runner() == 1) {
+        $runner_id = $userId;
+    } else {
+        $runner_id = 0;
+    }
+
+    $skillsArr = explode(', ', $skills);
+
+    $workitem->setRunnerId($runner_id);
+    $workitem->setProjectId($project_id);
+    $workitem->setStatus($status);
+    $workitem->setNotes($notes);
+    $workitem->setWorkitemSkills($skillsArr);
+    $workitem->setIs_bug($is_bug == 'true' ? 1 : 0);
+    $workitem->save();
+    $related = getRelated($notes);
+    Notification::massStatusNotify($workitem);
+
+    // if files were uploaded, update their workitem id
+    $file = new File();
+    // update images first
+    if (isset($fileUpload['images'])) {
+        foreach ($fileUpload['images'] as $image) {
+            $file->findFileById($image);
+            $file->setWorkitem($workitem->getId());
+            $file->save();
+        }
+    }
+    // update documents
+    if (isset($fileUpload['documents'])) {
+        foreach ($fileUpload['documents'] as $document) {
+            $file->findFileById($document);
+            $file->setWorkitem($workitem->getId());
+            $file->save();
+        }
+    }
+
+    if($is_bug && $bug_job_id > 0) {
+        //Load information about original job and notify
+        //users with fees and runner
+        Notification::workitemNotify(array(
+            'type' => 'bug_found',
+            'workitem' => $workitem,
+            'recipients' => array('runner', 'usersWithFeesBug')
+        ));
+        Notification::workitemSMSNotify(array(
+            'type' => 'bug_found',
+            'workitem' => $workitem,
+            'recipients' => array(
+                'runner', 
+                'usersWithFeesBug'
+            )
+        ));
+        $bugJournalMessage= " (bug of **#" . $workitem->getBugJobId() ."**)";
+    } else {
+        $bugJournalMessage= "";
+    }
+    
+    if (empty($_POST['itemid'])) {
+        $bid_fee_itemid = $workitem->getId();
+        $journal_message .= '**#' . $bid_fee_itemid . '** created by @' . $nick . ' ' . $bugJournalMessage;
+        $journal_message .= "\n\n**" . $summary . '**. Status set to *' . $status . '*';
+        if (!empty($_POST['files'])) {
+            $files = explode(',', $_POST['files']);
+            foreach ($files as $file) {
+                $sql = 'UPDATE `' . FILES . '` SET `workitem` = ' . $bid_fee_itemid . ' WHERE `id` = ' . (int)$file;
+                mysql_query($sql);
+            }
+        }
+    } else {
+        $bid_fee_itemid = $itemid;
+        $journal_message .= '#' . $bid_fee_itemid . ' updated by @' . $nick ;
+        $journal_message .=  "\n\n**" . $summary . '**. Status set to *' . $status . '*';
+    }
+    $journal_message .=  "$related. ";
+    if (!empty($_POST['invite'])) {
+        $people = explode(',', $_POST['invite']);
+        invitePeople($people, $workitem);
     }
 
     // don't send any journal notifications for DRAFTS
