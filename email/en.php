b783467069f211198448c1badd39baecf60e7e81
Lucas Crisman <kordero@gmail.com>
2014-03-28 20:51:52 -0300

    adding missed changes from last svn commits

--- email/en.php
+++ email/en.php
@@ -90,21 +90,19 @@ replacement data:
     'subject' => 'Get Started with Worklist',
     'body' => '
         Thank you for joining Worklist, {nickname}!<br/><br/>
-        Worklist offers fast pay for your work, an open codebase,
-        and great community. We\'re excited to have you join us as we
-        create a new way to get things done.<br/><br/>
+        We\'re excited to have you join us as we build a new virtual world platform.<br/><br/>
         Three things to help you get started:<br/><br/>
-           - Browse the source code*: <a href="http://svn.worklist.net">http://svn.worklist.net</a><br/>
-          - Get in the Journal to chat with us: <a href="' . JOURNAL_URL . '">' . JOURNAL_URL . '</a><br/>
-           - Bid on a project on our Worklist: <a href="' . WORKLIST_URL . '">' . WORKLIST_URL . '</a><br/>
+          - Browse the source code for our various projects (the primary one is hifi): <a href="https://github.com/highfidelity/">http://github.com/highfidelity</a><br/>
+          - Join us in our <a href="http://gitter.im/highfidelity/hifi">public chat room</a>.<br/>
+          - Check out our <a href="https://github.com/highfidelity/hifi/wiki/Coding-Standard">coding standards</a>.
         <br/>
-        Don\'t worry, you\'ll get a custom sandbox and support getting our codebase set up once your bid is accepted.
         <br/>
-        Thanks for joining and we hope to see you in the Journal soon!<br/><br/>
+        Thanks for joining, and we hope to see you committing code soon!
+        <br/>
+        <br/>
         Cheers,<br/><br/>
-        High Fidelity Inc<br/><br/>
-        p.s. Follow our continuing adventures...<br/>
-        Twitter: <a href="http://twitter.com/theworklist">http://twitter.com/worklistnet</a>
+        The High Fidelity Team<br/><br/>
+        P.S. Follow our continuing adventures: <a href="http://twitter.com/theworklist">@theworklist</a>
     '
 ),
 
