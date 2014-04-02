0617b566d0d75bb12858a4a8b0b3b917fa4184b4
Lucas Crisman <kordero@gmail.com>
2014-04-02 15:53:09 -0300

    Welcome message after email confirmation

--- controllers/Confirmation.php
+++ controllers/Confirmation.php
@@ -25,11 +25,28 @@ class ConfirmationController extends Controller {
 
                 // send welcome email
                 sendTemplateEmail($user->getUsername(), 'welcome', array('nickname' => $user->getNickname()), 'Worklist <contact@worklist.net>');
-                if (REQUIRELOGINAFTERCONFIRM) {
-                    Session::init(); // User must log in AFTER confirming (they're not allowed to before)
-                } else {
-                    LoginController::loginUser($user); //Optionally can login with confirm URL
-                }
+                LoginController::loginUser($user, false); //Optionally can login with confirm URL
+                $jobs = new JobsController();
+                $jobs->view->jumbotron = 
+                    "
+                        <h2>Welcome to the Worklist!</h2>
+                        <p>
+                            You can now browse through our open jobs and look for items that you'd like to work on for us!
+                        </p>
+                        <p>
+                            Before you can bid, you'll need to update your settings page with your payment details.  
+                            In addition, US Developers must upload a W9.  To navigate to your settings page, click 
+                            on your name in the upper right hand corner of the jobs page and choose settings.
+                        </p>
+                        <p>
+                            You can also chat with us and ask questions in our <a href=https://gitter.im/highfidelity/worklist>Gitter room.</a>.
+                        </p>
+                        <p>
+                            Happy Bidding and hope to see you commiting code soon!
+                        </p>                         
+                    ";
+                $jobs->run();
+                return;
             } else {
                 Utils::redirect('./signup');
             }
