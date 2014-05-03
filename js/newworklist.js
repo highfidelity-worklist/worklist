7e9328210ef820700d65087b22e580cec17ab648
Victor Argote <andrex289@hotmail.com>
2014-05-03 17:07:49 +0000

    Following CSSGuidelines

--- js/newworklist.js
+++ js/newworklist.js
@@ -14,37 +14,37 @@ var NewWorklist = {
         $(document).ajaxComplete(function() {
             $('body').removeClass('onAjax');
         });
+        
+        $(document).ready (function () {
+            $('.navbar').affix({
+                offset: {
+                top: function () { 
+                        return $('.navbar').outerHeight();
+                    }
+                }
+            });
+            
+            $('.navbar').on('affix.bs.affix', function () {
+                if ($('.dropdown').hasClass('open'))
+                    $('.dropdown-toggle').dropdown('toggle');
+                
+                if ($('.navbar-collapse').hasClass('in'))
+                    $('.navbar-collapse').collapse('toggle');
+                
+                $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+                 
+                /* Set a range between navbar height and navbar height and a half
+                   to show a nice fade in effect, this will only happens if the user
+                   scrolls slowly */
+                if ($('body').scrollTop() < $('.navbar').outerHeight() * 1.5) {
+                    $('.navbar').hide();
+                    $('.navbar').fadeIn("slow");
+                }
+            });
+
+            $('.navbar').on('affix-top.bs.affix', function () {
+                $('body').css('padding-top', '0px');
+            });
+        });
     }
 }
-
-$(document).ready (function () {
-    $('.navbar').affix({
-        offset: {
-        top: function () { 
-                return $('.navbar').outerHeight();
-            }
-        }
-    });
-    
-    $('.navbar').on('affix.bs.affix', function () {
-        if ($('.dropdown').hasClass('open'))
-            $('.dropdown-toggle').dropdown('toggle');
-        
-        if ($('.navbar-collapse').hasClass('in'))
-            $('.navbar-collapse').collapse('toggle');
-        
-        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
-         
-        /* Set a range between navbar height and navbar height and a half
-           to show a nice fade in effect, this will only happens if the user
-           scrolls slowly */
-        if ($('body').scrollTop() < $('.navbar').outerHeight() * 1.5) {
-            $('.navbar').hide();
-            $('.navbar').fadeIn("slow");
-        }
-    });
-
-    $('.navbar').on('affix-top.bs.affix', function () {
-        $('body').css('padding-top', '0px');
-    });
-});
