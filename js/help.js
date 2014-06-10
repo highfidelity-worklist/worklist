403cfa4e28fe1bab93d3beec6cdb4bff2527c422
Lucas Crisman <kordero@gmail.com>
2014-06-10 19:39:26 -0300

    Help sidebar improvements

--- js/help.js
+++ js/help.js
@@ -9,26 +9,36 @@ var Help = {
         $('#sidebar').on('affix.bs.affix', function() {
             $(this).css({'width': ''});
         });
-        $('#sidebar').on('affixed.bs.affix', Help.setAffixedSidebarWidth);
+        $('#sidebar').on('affixed.bs.affix', Help.refresh);
         if ($('#sidebar').is('.affix')) {
             Help.setAffixedSidebarWidth();
         }
 
         $('body').scrollspy({
             target: '#sidebar',
-            offset: 250
+            offset: 100
         });
 
         $('#sidebar a[href]').click(function(e) {
             e.preventDefault();
-            var scrollTo = $($(this).attr('href')).offset().top;
-            $('body').scrollTo(scrollTo);
+            var scrollTo = $($(this).attr('href')).offset().top - 70;
+            $('body').scrollTo(scrollTo, 400);
         });
 
-        window.addEventListener('resize', Help.setAffixedSidebarWidth, false);
+        window.addEventListener('resize', Help.refresh, false);
+
+        /**
+         * refreshing the affix two seconds since loaded seems to give better
+         * results with positioning issues, I guess that it might be cause by
+         * styling files not loaded already and thus scrollspy has outdated
+         * calculation results
+         * 10-JUN-2014 <kordero>
+         */
+        Help.refreshTimeout = setTimeout(Help.refresh, 2000);
     },
 
-    setAffixedSidebarWidth: function() {
+    refresh: function() {
         $('#sidebar').css({'width': $('#sidebar').parent().width() + 'px'});
+        $('body').scrollspy('refresh');
     }
 };
