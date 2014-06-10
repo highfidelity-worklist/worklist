51456ae6f8dafc0d807f953211e754a907c30d7c
Lucas Crisman <kordero@gmail.com>
2014-06-10 17:23:03 -0300

    Help: affix/scrollspy sidebar, first cut

---
+++ js/help.js
@@ -0,0 +1,32 @@
+var Help = {
+    init: function() {
+        $('#sidebar').affix({
+            offset: {
+                top: 150,
+                bottom: 300
+            }
+        });
+        $('#sidebar').on('affix.bs.affix', function() {
+            $(this).css({'width': ''});
+        });
+        $('#sidebar').on('affixed.bs.affix', Help.setAffixedSidebarWidth);
+        if ($('#sidebar').is('.affix')) {
+            Help.setAffixedSidebarWidth();
+        }
+
+        $('body').scrollspy({
+            target: '#sidebar',
+            offset: 100
+        });
+
+        $('#sidebar a[href]').click(function(e) {
+            e.preventDefault();
+            var scrollTo = $($(this).attr('href')).offset().top;
+            $('body').scrollTo(scrollTo);
+        });
+    },
+
+    setAffixedSidebarWidth: function() {
+        $('#sidebar').css({'width': $('#sidebar').parent().width() + 'px'});
+    }
+};
