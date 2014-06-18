d6964b67e429b4a1084c6b112509dbb6d29258bc
Lucas Crisman <kordero@gmail.com>
2014-06-18 15:00:29 -0300

    obsolete code removal in Fee:getSums

--- classes/Fee.class.php
+++ classes/Fee.class.php
@@ -110,123 +110,30 @@ class Fee
         }
     }
 
-    public static function getSums($type = '') {
+    public static function getSums() {
         $sum = array();
-        $sum["error"] = 0;
-
-        if ($type == "weekly") {
-            if (!empty($_SESSION['userid'])) {
-                $result = mysql_query("SELECT amount, worklist_id AS task 
-                                       FROM `".FEES."` 
-                                       WHERE `user_id` = {$_SESSION['userid']} AND 
-                                       `worklist_id` IN (SELECT `id` FROM `".WORKLIST."` WHERE `status` = 'Done') AND 
-                                       YEAR(DATE) = YEAR(NOW()) AND 
-                                       WEEK(`date`) = WEEK(NOW()) AND 
-                                       withdrawn != 1") or exit (mysql_error());
-                $output = '
-                <table class="table-bids">
-                  <thead>
-                    <tr class="table-hdng">
-                      <td>Task ID</td>
-                      <td>Fee</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                ';
-                $c = 0;
-                while($row = mysql_fetch_object($result)) {
-                    $c++;
-                    $output .= '
-                    <tr>
-                      <td><a href="./'.$row->task.'">'.$row->task.'</a></td>
-                      <td style="color: #6F6F6F; font-weight:bold;">$'.$row->amount.'</td>
-                    </tr>
-                    ';
-                }
-                if($c == 0){
-                    $output .= '
-                    <tr>
-                      <td colspan="2">There is nothing to show.</td>
-                    </tr>
-                    ';
-                }
-                $output .= '
-                  </tbody>
-                </table>
-                ';
-                $sum['output'] = $output;
+        if (getSessionUserId()) {
+            $r = mysql_query ("SELECT SUM(`amount`) AS `sum_amount` FROM `".FEES."` WHERE `user_id` = {$_SESSION['userid']} AND
+                              `worklist_id` IN (SELECT `id` FROM `".WORKLIST."` WHERE `status` = 'Done') AND YEAR(DATE) = YEAR(NOW()) AND
+                               MONTH(`date`) = MONTH(NOW()) AND withdrawn != 1;") or exit (mysql_error());
+            $sum['month'] = mysql_fetch_object($r)->sum_amount;
+            if (is_numeric($sum['month'])) {
+                $sum['month'] = money_format('%i', $sum['month']);
             } else {
-                $sum["error"] = 1;
+                $sum['month'] = '0.00';
             }
-        } else if ($type == "monthly") {
-            if (!empty($_SESSION['userid'])) {
-                $result = mysql_query("SELECT amount, worklist_id as task 
-                                       FROM `".FEES."` 
-                                       WHERE `user_id` = {$_SESSION['userid']} AND 
-                                       `worklist_id` IN (SELECT `id` FROM `".WORKLIST."` WHERE `status` = 'Done') AND 
-                                       YEAR(DATE) = YEAR(NOW()) AND 
-                                       MONTH(`date`) = MONTH(NOW()) AND 
-                                       withdrawn != 1") or exit (mysql_error());
-                $output = '
-                <table class="table-bids">
-                  <thead>
-                    <tr class="table-hdng">
-                      <td>Task ID</td>
-                      <td>Fee</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                ';
-                $c = 0;
-                while($row = mysql_fetch_object($result)) {
-                    $c++;
-                    $output .= '
-                    <tr>
-                      <td><a href="./'.$row->task.'">'.$row->task.'</a></td>
-                      <td style="color: #6F6F6F; font-weight:bold;">$'.$row->amount.'</td>
-                    </tr>
-                    ';
-                }
-                if($c == 0){
-                    $output .= '
-                    <tr>
-                      <td colspan="2">There is nothing to show.</td>
-                    </tr>
-                    ';
-                }
-                $output .= '
-                  </tbody>
-                </table>
-                ';
-                $sum['output'] = $output;
+            $r = mysql_query ("SELECT SUM(`amount`) AS `sum_amount` FROM `".FEES."` WHERE `user_id` = {$_SESSION['userid']} AND
+                              `worklist_id` IN (SELECT `id` FROM `".WORKLIST."` WHERE `status` = 'Done') AND YEAR(DATE) = YEAR(NOW()) AND
+                               WEEK(`date`) = WEEK(NOW()) AND withdrawn != 1;") or exit (mysql_error());
+            $sum['week'] = mysql_fetch_object($r)->sum_amount;
+            if (is_numeric($sum['week'])) {
+                $sum['week'] = money_format('%i', $sum['week']);
             } else {
-                $sum["error"] = 1;
+                $sum['week'] = '0.00';
             }
         } else {
-            if (!empty($_SESSION['userid'])) {
-                $r = mysql_query ("SELECT SUM(`amount`) AS `sum_amount` FROM `".FEES."` WHERE `user_id` = {$_SESSION['userid']} AND
-                                  `worklist_id` IN (SELECT `id` FROM `".WORKLIST."` WHERE `status` = 'Done') AND YEAR(DATE) = YEAR(NOW()) AND
-                                   MONTH(`date`) = MONTH(NOW()) AND withdrawn != 1;") or exit (mysql_error());
-                $sum['month'] = mysql_fetch_object($r)->sum_amount;
-                if (is_numeric($sum['month'])) {
-                    $sum['month'] = number_format($sum['month']);
-                } else {
-                    $sum['month'] = '0';
-                }
-            
-                $r = mysql_query ("SELECT SUM(`amount`) AS `sum_amount` FROM `".FEES."` WHERE `user_id` = {$_SESSION['userid']} AND
-                                  `worklist_id` IN (SELECT `id` FROM `".WORKLIST."` WHERE `status` = 'Done') AND YEAR(DATE) = YEAR(NOW()) AND
-                                   WEEK(`date`) = WEEK(NOW()) AND withdrawn != 1;") or exit (mysql_error());
-                $sum['week'] = mysql_fetch_object($r)->sum_amount;
-                if (is_numeric($sum['week'])) {
-                    $sum['week'] = number_format($sum['week']);
-                } else {
-                    $sum['week'] = '0';
-                }
-            } else {
-                $sum['month'] = '0';
-                $sum['week'] = '0';
-            }
+            $sum['month'] = '0.00';
+            $sum['week'] = '0.00';
         }
         return $sum;
     }
