434e045b214966420d5a489a6c44169f24d56b1f
Lucas Crisman <kordero@gmail.com>
2014-07-16 17:44:57 -0300

    deleting logo remove option from project edit

--- controllers/Project.php
+++ controllers/Project.php
@@ -86,9 +86,6 @@ class ProjectController extends Controller {
             if ($_REQUEST['logoProject'] != "") {
                 $project->setLogo(basename($_REQUEST['logoProject']));
             }
-            if (isset($_REQUEST['noLogo']) && $_REQUEST['noLogo'] == "1") {
-                $project->setLogo("");
-            }
             $project->save();
             // we clear post to prevent the page from redirecting
             $_POST = array();
