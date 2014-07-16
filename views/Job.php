11c49c8d38be579a3684cb7ab461806ffac25837
Lucas Crisman <kordero@gmail.com>
2014-07-16 12:23:32 -0300

    obsolete css files removal

--- views/Job.php
+++ views/Job.php
@@ -6,9 +6,7 @@ class JobView extends View {
     public $title = '%d: %s - Worklist';
 
     public $stylesheets = array(
-        'css/legacy/workitem.css',
         'css/favorites.css',
-        'css/budget.css',
         'css/entries.css',
         'css/job.css'
    );
